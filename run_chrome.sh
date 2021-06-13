c5d6d16bc407b6bd9629c5145b9093b00b484429
Gavin Mogan <git@gavinmogan.com>
2021-06-13 15:01:46 -0700

    Add some flags from https://github.com/andreasonny83/lighthouse-ci/blob/main/lib/config.js

--- run_chrome.sh
+++ run_chrome.sh
@@ -4,4 +4,6 @@ exec google-chrome --user-data-dir=$TMP_PROFILE_DIR \
     --start-maximized \
     --no-first-run \
     --no-sandbox \
+    --disable-gpu \
+    --no-zygote \
     --remote-debugging-port=9222 'about:blank'
