69f5aa9d8df68c6457043fd24e4b325cccfd99b7
Zbynek Konecny <zbynek@geogebra.org>
2026-09-09 02:20:07 +0200

    Run CI on Java 21 and 25

--- Jenkinsfile
+++ Jenkinsfile
@@ -2,4 +2,9 @@
  See the documentation for more options:
  https://github.com/jenkins-infra/pipeline-library/
 */
-buildPlugin(useContainerAgent: true)
+buildPlugin(
+  useContainerAgent: true,
+  configurations: [
+    [platform: 'linux', jdk: 25],
+    [platform: 'windows', jdk: 21],
+])
--- src/main/java/io/jenkins/plugins/LighthouseReportBuildAction.java
+++ src/main/java/io/jenkins/plugins/LighthouseReportBuildAction.java
@@ -2,7 +2,6 @@ package io.jenkins.plugins;
 
 import hudson.model.Action;
 
-import org.apache.commons.lang.StringUtils;
 import org.kohsuke.stapler.StaplerRequest;
 import org.kohsuke.stapler.StaplerResponse;
 
@@ -38,7 +37,7 @@ public class LighthouseReportBuildAction implements Action, Serializable {
     @CheckForNull
     @Override
     public String getDisplayName() {
-        if (StringUtils.isNotEmpty(name)) {
+        if (name != null && !name.isEmpty()) {
             return Messages.LighthouseReportBuildAction_DisplayName_Configured(name);
         }
         return Messages.LighthouseReportBuildAction_DisplayName();
@@ -47,7 +46,7 @@ public class LighthouseReportBuildAction implements Action, Serializable {
     @CheckForNull
     @Override
     public String getUrlName() {
-        if (StringUtils.isNotEmpty(name)) {
+        if (name != null && !name.isEmpty()) {
             return "lighthousereport_" + sanitizeName(name);
         }
         return "lighthousereport";
